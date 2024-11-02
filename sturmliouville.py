2b7b15c9bcfa744ee4a8a5173a50973e84a3af1d
sarsaeroth <pgclark@iastate.edu>
2024-11-01 18:14:29 -0600

    First checkin

---
+++ sturmliouville.py
@@ -0,0 +1,13 @@
+class SturmLiouville():
+
+    def __init__(self):
+
+    def p(function_data):
+        #(x,y) coordinates of p(x) as a key-value dictionary
+
+    def q(function_data):
+        #(x,y) coordinates of q(x) as a key-value dictionary
+
+    def w(function_data):
+        #(x,y) coordinates of w(x) as a key-value dictionary
+
